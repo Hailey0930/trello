6e8092590328a7dcd29ba01c87dc5ba7a3ac9718
Hailey0930 <hyerin0930@gmail.com>
2024-07-12 19:59:21 +0900

    :lipstick: Add Dashboard style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,20 @@
 import { Category } from "../_types/Category";
 
 function DashboardPage() {
-  const categoryList: Category[] = [{ id: "1", title: "title", cards: [] }];
+  const categoryList: Category[] = [
+    { id: "1", title: "title", cards: [] },
+    { id: "1", title: "title2", cards: [] },
+  ];
 
   return (
-    <div>
+    <div className="flex">
       {categoryList.map((category) => (
-        <div key={category.id}>{category.title}</div>
+        <div
+          key={category.id}
+          className="w-1/2 border-solid border-2 border-sky-500"
+        >
+          {category.title}
+        </div>
       ))}
     </div>
   );
