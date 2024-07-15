bb5384858cee8a81373ade1eed3d5bafcab88935
hyerin <juju0930@uzurotech.com>
2024-07-15 09:28:24 +0900

    :sparkles: Add FixtureFactory

--- src/app/_data/categoryRepository.ts
+++ src/app/_data/categoryRepository.ts
@@ -1,14 +1,18 @@
+import { FixtureFactory } from "@reflow-work/test-fixture-factory";
 import { Category } from "../_types/Category";
 
 export interface CategoryRepository {
   getAll: () => Category[];
 }
 
+const categoryFactory = new FixtureFactory<Category>(() => {
+  return { id: "1", title: "title", cards: [] };
+});
+
+const [category1, category2] = categoryFactory.createList(2);
+
 const getAll = () => {
-  return [
-    { id: "1", title: "title", cards: [] },
-    { id: "2", title: "title2", cards: [] },
-  ];
+  return [category1, category2];
 };
 
 export const categoryRepository: CategoryRepository = {
