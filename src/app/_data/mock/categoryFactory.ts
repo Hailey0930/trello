e4f2cd867dbdaf97308067542c419544fbd4e9de
hyerin <juju0930@uzurotech.com>
2024-07-29 18:04:40 +0900

    :bug: Fix categoryFactory bug

--- src/app/_data/mock/categoryFactory.ts
+++ src/app/_data/mock/categoryFactory.ts
@@ -3,8 +3,19 @@ import { faker } from "@faker-js/faker";
 import { FixtureFactory } from "@reflow-work/test-fixture-factory";
 
 const categoryFactory = new FixtureFactory<Category>(() => {
-  return { id: faker.string.uuid(), title: faker.word.words(), cards: [] };
+  return {
+    id: faker.string.uuid(),
+    title: faker.word.words(),
+    order: 0,
+    cards: [],
+  };
 });
 
-const mockCategoryList = categoryFactory.createList(2);
+const mockCategoryList = categoryFactory
+  .createList(2)
+  .map((category, index) => ({
+    ...category,
+    order: index,
+  }));
+
 export default mockCategoryList;
