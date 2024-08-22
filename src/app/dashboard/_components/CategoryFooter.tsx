9a2c5a84373fdcbe96997d28a5ab2b19b9d77b08
hyerin <juju0930@uzurotech.com>
2024-08-22 15:52:24 +0900

    :lipstick: Add template design

--- src/app/dashboard/_components/CategoryFooter.tsx
+++ src/app/dashboard/_components/CategoryFooter.tsx
@@ -1,16 +1,30 @@
-import { PlusOutlined } from "@ant-design/icons";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import {
+  ArrowLeftOutlined,
+  CloseOutlined,
+  CopyOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
 import { Input } from "antd";
 import { useRef, useState } from "react";
 import { CategoryFooterProps } from "@/app/_types/Category";
 import useClickOutside from "../_hooks/useClickOutside";
+import Card from "./Card";
 
 function CategoryFooter({ onSaveCard }: CategoryFooterProps) {
   const [isAddingCard, setIsAddingCard] = useState(false);
   const [newCardTitle, setNewCardTitle] = useState("");
+  const [isTemplateModalVisible, setIsTemplateModalVisible] = useState(false);
+  const [isSelectingTemplate, setIsSelectingTemplate] = useState(false);
 
   const addCardRef = useRef<HTMLDivElement>(null);
+  const templateModalRef = useRef<HTMLDivElement>(null);
+  const selectTemplateRef = useRef<HTMLDivElement>(null);
 
   useClickOutside(setIsAddingCard, addCardRef);
+  useClickOutside(setIsTemplateModalVisible, templateModalRef);
+  useClickOutside(setIsSelectingTemplate, selectTemplateRef);
 
   const handleAddCard = async () => {
     setIsAddingCard(true);
@@ -27,8 +41,30 @@ function CategoryFooter({ onSaveCard }: CategoryFooterProps) {
     setNewCardTitle("");
   };
 
+  const handleTemplate = () => {
+    setIsTemplateModalVisible(!isTemplateModalVisible);
+  };
+
+  const handleCloseTemplateModal = () => {
+    setIsTemplateModalVisible(false);
+  };
+
+  const handleSelectTemplate = () => {
+    setIsSelectingTemplate(true);
+    setIsTemplateModalVisible(false);
+  };
+
+  const handleBackToTemplate = () => {
+    setIsSelectingTemplate(false);
+    setIsTemplateModalVisible(true);
+  };
+
+  const handleCloseSelectTemplate = () => {
+    setIsSelectingTemplate(false);
+  };
+
   return (
-    <footer className="p-2">
+    <footer className="relative p-2 flex justify-between items-center">
       <button
         type="button"
         className="flex items-center gap-2 text-sm bg-sky-300 text-white px-2 py-1 rounded-lg hover:bg-sky-400"
@@ -36,6 +72,13 @@ function CategoryFooter({ onSaveCard }: CategoryFooterProps) {
       >
         <PlusOutlined />
         <div> Add Card</div>
+      </button>
+      <button
+        type="button"
+        className=" rounded-full px-1 hover:bg-gray-200"
+        onClick={handleTemplate}
+      >
+        <CopyOutlined style={{ color: "#5c5b5b" }} />
       </button>
       {isAddingCard && (
         <div className="flex flex-col gap-2 mt-2" ref={addCardRef}>
@@ -61,6 +104,68 @@ function CategoryFooter({ onSaveCard }: CategoryFooterProps) {
               Cancel
             </button>
           </div>
+        </div>
+      )}
+      {isTemplateModalVisible && (
+        <div
+          className="absolute left-full top-5 z-10 bg-white p-2 w-64 rounded-lg shadow-md"
+          ref={templateModalRef}
+        >
+          <div className="relative flex flex-col justify-center items-center mb-5 gap-2">
+            <h1 className="font-semibold">Card Templates</h1>
+            <button
+              type="button"
+              className="absolute right-0 top-0 rounded-full px-1 hover:bg-gray-200"
+              onClick={handleCloseTemplateModal}
+            >
+              <CloseOutlined style={{ color: "#5c5b5b" }} />
+            </button>
+            <div className="flex flex-col justify-center items-center gap-2 w-full">
+              <div className="w-full" onClick={handleSelectTemplate}>
+                <Card title="test" type="template" />
+              </div>
+            </div>
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2 mt-2 w-full px-1 hover:bg-gray-200"
+            >
+              <PlusOutlined style={{ color: "#5c5b5b" }} />
+              Create a new template
+            </button>
+          </div>
+        </div>
+      )}
+      {isSelectingTemplate && (
+        <div
+          className="absolute left-full top-5 z-10 bg-white p-2 w-64  rounded-lg shadow-md"
+          ref={selectTemplateRef}
+        >
+          <div className="relative flex flex-col justify-center items-center mb-5 gap-2">
+            <div className="flex justify-between items-center w-full">
+              <button
+                type="button"
+                className="rounded-full px-1 hover:bg-gray-200"
+                onClick={handleBackToTemplate}
+              >
+                <ArrowLeftOutlined style={{ color: "#5c5b5b" }} />
+              </button>
+              <h1 className="font-semibold">Create card</h1>
+              <button
+                type="button"
+                className="rounded-full px-1 hover:bg-gray-200"
+                onClick={handleCloseSelectTemplate}
+              >
+                <CloseOutlined style={{ color: "#5c5b5b" }} />
+              </button>
+            </div>
+            <Input type="text" placeholder="Card name" />
+          </div>
+          <button
+            type="button"
+            className="w-full flex justify-center items-center text-sm bg-sky-300 text-white px-2 py-1 rounded-lg hover:bg-sky-400"
+          >
+            Create card
+          </button>
         </div>
       )}
     </footer>
